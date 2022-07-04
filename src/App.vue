7515ec016e87a5523659b77d0eb118385e373efb
偏执 <1924618143@qq.com>
2022-07-04 20:17:18 +0800

    创建

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Book>
+      <tbody>
+        <tr></tr>
+      </tbody>
+    </Book>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Book from './components/Book.vue';
 export default {
-  name: 'App',
+  data() {
+    return {};
+  },
   components: {
-    HelloWorld
-  }
-}
+    Book,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Book.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>获取图书信息</p>
+    <button @click="getBook">查看信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      Objbook: {
+        bookname: '',
+        author: '',
+        publisher: '',
+        id: '',
+      },
+    };
+  },
+
+  methods: {
+    getBook() {
+      this.$axios({
+        url: '/api/getbooks',
+        method: 'GET',
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
