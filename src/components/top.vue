af5385683709e601c7b000e89a54fe02c07e378f
lcydr <1211753823@qq.com>
2022-07-04 20:12:49 +0800

    新建

---
+++ src/components/top.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <table class="book" border="1" cellspacing="0" cellpadding="0">
+      <tr>
+        <th>序号</th>
+        <th>书名</th>
+        <th>作者</th>
+        <th>出版商</th>
+        <th>操作</th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+#app {
+  width: 1000px;
+  height: 1000px;
+  margin: 0 auto;
+  background: pink;
+}
+</style>
